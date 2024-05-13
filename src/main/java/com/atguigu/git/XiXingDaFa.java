63a550dcedef29e292c3d1c768fd5cd46359f7c9
hxh <2792636790@qq.com>
2024-05-13 19:59:22 +0800

    创建吸星大法

---
+++ src/main/java/com/atguigu/git/XiXingDaFa.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+public class XiXingDaFa {
+    public static void main(String[] args) {
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+    }
+}
