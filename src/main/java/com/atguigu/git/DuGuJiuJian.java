5a9e5d879a5dc32128c3e2e95f05396b941f74eb
hxh <2792636790@qq.com>
2024-05-13 19:01:14 +0800

    创建独孤九剑

---
+++ src/main/java/com/atguigu/git/DuGuJiuJian.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+public class DuGuJiuJian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
